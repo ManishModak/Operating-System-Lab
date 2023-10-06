715ee6cc0cf984e97bc76904cccc8c164af0060e
Manish <111450082+ManishModak@users.noreply.github.com>
2023-10-06 12:35:33 +0530

    Update a.c

--- Lab7/A/a.c
+++ Lab7/A/a.c
@@ -10,10 +10,17 @@ int main() {
 
 	int fd ;
 	
+	char * myfile = "/home/student/TA45/file.txt" ;
 	
-	char * myfifo = "/tmp/myfifo" ;
+	FILE *fp ;
 	
-	mkfifo(myfifo,0666) ;
+	char * myfifo1 = "/home/student/TA45/myfifo1" ;
+	
+	char * myfifo2 = "/home/student/TA45/myfifo2" ;
+	
+	mkfifo(myfifo1,0666) ;
+	
+	mkfifo(myfifo2,0666) ;
 	
 	char arr1[80] , arr2[80] ;
 	
@@ -21,29 +28,23 @@ int main() {
 	
 		int i = 0 , wordCount = 0 , lineCount = 0 , charCount = 0 ;
 		
-		fd = open(myfifo,O_WRONLY) ;
+		
+		
+		//Writing in fifo1
+		
+		fd = open(myfifo1,O_WRONLY) ;
 		
 		fgets(arr2,80,stdin);
 		
 		write(fd,arr2,strlen(arr2)+1);
 		
-		close(fd);
-		
-		fd = open(myfifo,O_RDONLY);
-		
-		read(fd,arr1,sizeof(arr1));
-		
-		printf("User 2: %s \n",arr1);
-		
-		close(fd);
-		
-		while(i < strlen(arr1)) {
+		while(i < strlen(arr2)) {
 			
-			if(arr1[i] == ' ')
-			{	 
+			if(arr2[i] == ' ')
+			{
 				wordCount++ ;
 			}
-			else if(arr1[i] == '\n')
+			else if(arr2[i] == '\n')
 			{
 				wordCount++ ;
 				lineCount++ ;
@@ -56,7 +57,61 @@ int main() {
 			i++;
 		}
 		
-		printf("wordCount: %d,lineCount: %d,charCount: %d \n",wordCount,lineCount,charCount) ;
+		close(fd);
+		
+		
+	
+		
+		//writing in fifo2
+		
+		char tempstr[100];
+		sprintf(tempstr, "wordCount: %d, lineCount: %d, charCount: %d\n", wordCount, lineCount, charCount);
+
+			
+		fp = fopen(myfile,"w");
+		
+		if(fp) 
+		{
+			fputs(tempstr, fp);
+            fclose(fp);
+		}
+		else
+		{
+			printf("Error Opening the File");
+		}
+		
+		fd = open(myfifo2,O_WRONLY) ;
+		
+		write(fd,tempstr,strlen(tempstr)+1);
+		
+		close(fd);
+		
+		
+		
+		//Reading From fifo1
+		
+		fd = open(myfifo1,O_RDONLY);
+		
+		read(fd,arr1,sizeof(arr1));
+		
+		printf("User 2: %s \n",arr1);
+		
+		close(fd);
+		
+		
+		
+		//Reading From fifo2
+		
+		char countData[100] ;
+		
+		fd = open(myfifo2,O_RDONLY) ;
+		
+		read(fd,countData,sizeof(countData));
+		
+		printf("User CountData: %s \n",countData);
+		
+		close(fd);
+		
 	}
 	
 	
@@ -64,11 +119,16 @@ int main() {
 }
 
 
-----------------------------------------------------------------------
-	
-student@student:~/Downloads$ gcc a.c -o a
-student@student:~/Downloads$ ./a
-Hi
-User 2: hello myself Donald
+
+
+/*
+
+student@student:~$ gcc a.c -o a
+student@student:~$ ./a
+
+Hi Im Manish
+User 2: Hello Myself zein
  
-wordCount: 3,lineCount: 1,charCount: 17 
+User CountData: wordCount: 3, lineCount: 1, charCount: 15
+
+*/
